e21954357aecec52d38ffab231c52139170ad5d4
Dmnt <dmnt42@gmail.com>
2014-05-12 19:25:34 +0200

    prova

--- AfroditaNuvies/src/net/infobosccoma/projecte/afroditanuvies/MenuPrincipal.java
+++ AfroditaNuvies/src/net/infobosccoma/projecte/afroditanuvies/MenuPrincipal.java
@@ -3,22 +3,29 @@ package net.infobosccoma.projecte.afroditanuvies;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
-public class MenuPrincipal extends Activity {
+public class MenuPrincipal extends Activity implements OnItemClickListener {
 	ListView llista;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_llista_opcions);
-		llista=(ListView) findViewById(R.id.listView1);
-		String[] values = {getResources().getString(R.string.vestitsnuvia), getResources().getString(R.string.prendremides),getResources().getString(R.string.quisom), getResources().getString(R.string.dadescontacte)};
-		String[] values2 = {"ic_launcher","ic_launcher","ic_launcher","ic_launcher"};
-		ElementsmenuAdapter adapter = new ElementsmenuAdapter(this, values, values2);
+		llista = (ListView) findViewById(R.id.listView1);
+		String[] values = { getResources().getString(R.string.vestitsnuvia),
+				getResources().getString(R.string.prendremides),
+				getResources().getString(R.string.quisom),
+				getResources().getString(R.string.dadescontacte) };
+		String[] values2 = { "ic_launcher", "ic_launcher", "ic_launcher",
+				"ic_launcher" };
+		ElementsmenuAdapter adapter = new ElementsmenuAdapter(this, values,
+				values2);
 		llista.setAdapter(adapter);
-		//TODO continuar, mirar si la llista es veu almenys
 	}
 
 	@Override
@@ -28,4 +35,21 @@ public class MenuPrincipal extends Activity {
 		return true;
 	}
 
+	@Override
+	public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+		switch (position){
+		case 0:
+			//TODO implementar opció
+			break;
+		case 1:
+			//TODO implementar opció
+			break;
+		case 2:
+			//TODO implementar opció
+			break;
+		case 3:
+			//TODO implementar opció
+			break;
+		}
+	}
 }
